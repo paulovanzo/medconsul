8b333bf6a378b19dcd1eae11fe3d53ffde5b3c9e
José de Sousa <zedsousa@DESKTOP-C224KA6.localdomain>
2022-10-16 20:37:17 -0300

    Fazer crud de paciente

--- backend/src/main/java/app/medconsul/entities/Patient.java
+++ backend/src/main/java/app/medconsul/entities/Patient.java
@@ -19,9 +19,76 @@ public class Patient {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     public String name;
 
-    @Column
+    @Column(nullable = false)
     public String email;
+
+    @Column(nullable = false)
+    public String phoneNumber;
+
+    @Column
+    public String urlImage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Patient patient = (Patient) object;
+        return id.equals(patient.id);
+    }
+
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Patient{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
--- backend/src/main/java/app/medconsul/repositories/IPatientRepository.java
+++ backend/src/main/java/app/medconsul/repositories/IPatientRepository.java
@@ -1,9 +1,10 @@
 package app.medconsul.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import app.medconsul.entities.Doctor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import app.medconsul.entities.Patient;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "patients", path = "patients" )
 public interface IPatientRepository extends JpaRepository<Patient,Long>{
 }
